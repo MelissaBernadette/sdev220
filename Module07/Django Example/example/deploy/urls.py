5c23e3d01dbc4dbdd7d2cc0236bf59b9a7c5585c
Melissa Haney <mhaney29@ivytech.edu>
2022-07-24 00:00:40 -0400

    Django Update

--- Module07/Django Example/example/deploy/urls.py
+++ Module07/Django Example/example/deploy/urls.py
@@ -1,9 +1,23 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
+from django.contrib import admin
+
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path("homePage/", views.Home),
     path("otherPage/", views.Other),
-    path("post_list/", views.post_list)
+    path("post_list/", views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('post/new/', views.post_new, name='post_new'),
+    path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
+    path('accounts/logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
+    path('admin/', admin.site.urls),
+    path('drafts/', views.post_draft_list, name='post_draft_list'),
+    path('post/<pk>/publish/', views.post_publish, name='post_publish'),
+    path('post/<pk>/remove/', views.post_remove, name='post_remove'),
+    path('post/<int:pk>/comment/', views.add_comment_to_post, name='add_comment_to_post'),
+    path('comment/<int:pk>/approve/', views.comment_approve, name='comment_approve'),
+    path('comment/<int:pk>/remove/', views.comment_remove, name='comment_remove'),
 ]
 
